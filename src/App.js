4f5cea4d902f40b334842aeebdc02322584ee950
Saimun Hossain <saimun_hossain@outlook.com>
2020-07-29 15:48:47 +0600

    seperated footer from home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
