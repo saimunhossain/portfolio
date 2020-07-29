315d4687698c3c0cf17f2b34666928aa8f7ec0ed
Saimun Hossain <saimun.singularity@gmail.com>
2020-07-29 21:05:27 +0600

    Updates

---
+++ precache-manifest.2e3c05a1d16386c5bc015af6af6dd7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9fec5e49c1dbe7fc5a9f01779dc8841",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e7e78333c64585f0c70f",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edbe62d9c7e518133d8a",
+    "url": "/portfolio/static/js/2.c93d5626.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.c93d5626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e78333c64585f0c70f",
+    "url": "/portfolio/static/js/main.7255221c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
